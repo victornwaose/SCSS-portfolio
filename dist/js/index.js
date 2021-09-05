0a54d5a2a0d04649056331fe6b7b8924d47c15e0
nwaose victor <nwaosevictor1234@gmail.com>
2021-09-05 15:23:55 +0100

    added about.html

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn_burger");
  const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav")
+const navItem = document.querySelectorAll(".menu-nav_item")
 
 let showMenu = false;
   
@@ -13,12 +14,15 @@ function toggleMenu() {
         hamburger.classList.add("open");
         nav.classList.add("open");
         menuNav.classList.add("open");
+        navItem.forEach(item => item.classList.add("open"))
 
         showMenu= true
     } else{
         hamburger.classList.remove("open");
         nav.classList.remove("open");
         menuNav.classList.remove ("open");
+        navItem.forEach(item => item.classList.remove("open"))
+
         showMenu= false
     }
 }
